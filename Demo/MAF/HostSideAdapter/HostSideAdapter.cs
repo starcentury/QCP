01c77b453a9dc380434e168fc01ed0f347661c62
Akini <Akini@126.com>
2013-12-02 12:29:59 +0800

    MAF

---
+++ Demo/MAF/HostSideAdapter/HostSideAdapter.cs
@@ -0,0 +1,20 @@
+﻿using System.AddIn.Pipeline;
+
+namespace MAF
+{
+    [HostAdapter()]
+    public class HostSideAdapter : HostSideView
+    {
+        private IContract _contract;
+        private System.AddIn.Pipeline.ContractHandle _handle;
+        public HostSideAdapter(IContract contract)
+        {
+            this._contract = contract;
+            this._handle = new ContractHandle(contract);
+        }
+        public string Say()
+        {
+            return this._contract.Say();
+        }
+    }
+}
