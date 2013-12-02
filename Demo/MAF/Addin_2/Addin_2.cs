96f4a2749b3ff22ef994a3b549b7c0940689dc30
Akini <Akini@126.com>
2013-12-02 15:37:22 +0800

    添加Addin2

---
+++ Demo/MAF/Addin_2/Addin_2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.AddIn;
+using System.AddIn.Pipeline;
+
+namespace MAF
+{
+    [AddIn("Addin_2", Description = "this is Addin_2", Publisher = "Addin_2", Version = "1.0")]
+    public class Addin_2 : AddinSideView
+    {
+        public string Say()
+        {
+            return "Addin_2";
+        }
+    }
+}
